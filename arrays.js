4b50d56dae3bae66be349bba147fad0b50a91b39
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-02 13:14:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-
 function addElementToBeginningOfArray(array,element){
  var array_plus = [array,element[0]]
  return array_plus
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  var array =[]
+  array.unshift(element)
 }
